4dbfc004f57ee70cf63b2f63ee754ecd19605b9d
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-30 22:59:17 +0900

    Enhance SQL comments and clarify DOCTOR table structure
    Added additional explanation for the DOCTOR table and included notes on DATE_FORMAT usage.

---
+++ programmers/SELECT/MySQL/132203.sql
@@ -0,0 +1,19 @@
+/*
+# 흉부외과 또는 일반외과 의사 목록 출력하기
+
+# 132203 MySQL
+
+# 문제
+# DOCTOR 테이블에서 진료과가 흉부외과(CS)이거나 일반외과(GS)인 의사의 이름, 의사ID, 진료과, 고용일자를 조회하는 SQL문을 작성해주세요. 이때 결과는 고용일자를 기준으로 내림차순 정렬하고, 고용일자가 같다면 이름을 기준으로 오름차순 정렬해주세요.
+
+DOCTOR 테이블은 다음과 같으며 DR_NAME, DR_ID, LCNS_NO, HIRE_YMD, MCDP_CD, TLNO는 각각 의사이름, 의사ID, 면허번호, 고용일자, 진료과코드, 전화번호를 나타냅니다.
+
+*/
+
+SELECT DR_NAME, DR_ID, MCDP_CD, DATE_FORMAT(HIRE_YMD, '%Y-%m-%d') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
+
+-- 잊지말자. MySQL은 DATE_FORMAT!
+-- DATE_FORMAT(HIRE_YMD, '%Y-%m-%d')에서 %M, %D로 작성할 시 숫자가 아닌 글자 형태로 출력
