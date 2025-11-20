fea1122b058842e319d81d71bdd93efd7473936d
Daon Kang <gracekang253@koreacu.ac.kr>
2025-11-20 18:11:17 +0900

    Update 133024.sql
    /* 주석 반대로 달아버려서 재 수정하여 커밋

--- programmers/SELECT/MySQL/133024.sql
+++ programmers/SELECT/MySQL/133024.sql
@@ -1,4 +1,4 @@
-*/
+/*
 # 인기있는 아이스크림
 
 # 133024 MySQL
@@ -8,7 +8,7 @@
 
 # FIRST_HALF 테이블은 아이스크림 가게의 상반기 주문 정보를 담은 테이블
 # SHIPMENT_ID, FLAVOR, TOTAL_ORDER는 각각 아이스크림 공장에서 아이스크림 가게까지의 출하 번호, 아이스크림 맛, 상반기 아이스크림 총주문량
-/*
+*/
 
 SELECT FLAVOR 
 FROM FIRST_HALF
